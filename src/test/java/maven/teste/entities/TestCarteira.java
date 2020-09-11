a4c6b77f59d12d0b92598c08a41bb3a5f5e19e06
ThiagoBuen <thiagobueno@gea.inatel.br>
2020-09-11 11:12:41 -0300

    11/09 - Designed test units {Carteira} :rocket:

---
+++ src/test/java/maven/teste/entities/TestCarteira.java
@@ -0,0 +1,34 @@
+package maven.teste.entities;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import model.entities.Carteira;
+
+public class TestCarteira {
+	
+	Carteira carteira;
+	
+	@Before
+	public void beforeTeste()
+	{
+		carteira = new Carteira("Rico Investimento");
+	}
+	
+	@Test
+	public void testeExisteCarteira()
+	{
+		assertNotNull(carteira);	
+	}
+	
+	@Test
+	public void testeCarteiraVazia()
+	{	
+		int qtdInvestimentos = carteira.getInvestimentos().size();
+		
+		assertEquals(0, qtdInvestimentos);
+	}
+}
